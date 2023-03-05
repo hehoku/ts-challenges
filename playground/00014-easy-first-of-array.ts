925a8619521ee57b013d10411caf13da22860834
hehoku <hehoku@outlook.com>
2023-03-05 23:01:00 +0800

    add: extends

--- playground/00014-easy-first-of-array.ts
+++ playground/00014-easy-first-of-array.ts
@@ -22,7 +22,14 @@
 
 /* _____________ Your Code Here _____________ */
 
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0]
+
+/**
+extends 关键字的作用：
+这里的作用是判断泛型类型 T 是否是一个数组类型，如果是空数组类型 []，则返回 never
+类型，表示不存在第一个元素；否则返回数组的第一个元素类型。这个条件类型的作用是在
+编译时对传入的泛型类型进行约束，从而保证类型的正确性。
+ */
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
