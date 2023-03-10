72d72a65ca54636b5a77e77fa4b1b5f970dcb820
hehoku <hehoku@outlook.com>
2023-03-10 20:31:07 +0800

    add: use infer to implement Pop

--- playground/00016-undefined-pop.ts
+++ playground/00016-undefined-pop.ts
@@ -26,7 +26,12 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer R, infer _]
+  ? R
+  : T extends []
+    ?
+      []
+    : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
