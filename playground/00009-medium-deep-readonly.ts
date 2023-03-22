15cf6d3c4f81d606dc5e8ee5bb095d30487a51b2
hehoku <hehoku@outlook.com>
2023-03-22 10:10:23 +0800

    add: deep readonly

--- playground/00009-medium-deep-readonly.ts
+++ playground/00009-medium-deep-readonly.ts
@@ -36,7 +36,25 @@
 
 /* _____________ Your Code Here _____________ */
 
-type DeepReadonly<T> = any
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends (...args: any[]) => any
+    ? T[K]
+    : T[K] extends object
+      ? DeepReadonly<T[K]>
+      : T[K]
+}
+
+/**
+这段代码定义了一个类型别名 `DeepReadonly<T>`，它将传入的泛型类型 `T` 中所有属性变
+为只读（readonly）。其中使用了条件语句和递归调用自身来实现对嵌套对象的处理。具体地
+说，对于 T 的每个属性 K，在新的类型中都会创建一个同名、但是只读的属性，并且根据该
+属性值是否为函数或对象分别进行不同的处理：
+
+- 如果该属性值是函数，则保持不变；
+- 如果该属性值是对象，则递归调用 DeepReadonly 对其进行深度只读化。
+
+最终返回一个完全只读化后的新类型。
+ */
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
